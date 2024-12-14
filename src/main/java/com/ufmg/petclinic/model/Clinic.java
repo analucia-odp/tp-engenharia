04217b0c99b5999cd25f3a7b585d8ba9b6320f63
Ana Sossai <analuizadsossai@hotmail.com>
2024-12-13 21:28:45 -0300

    feat: create class Clinic

---
+++ src/main/java/com/ufmg/petclinic/model/Clinic.java
@@ -0,0 +1,34 @@
+package com.ufmg.petclinic.model;
+
+public class Clinic {
+
+    private Long cnpj;
+    private String nome;
+    private String endereco;
+
+   
+    public Long getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(Long cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
+
