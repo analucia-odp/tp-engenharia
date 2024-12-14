cf8b1a89d0d3d08ee709bf6def9d016658f3b569
analucia-odp <analucia.odp@gmail.com>
2024-12-14 15:00:27 -0300

    feat: create user repository

---
+++ src/main/java/com/ufmg/petclinic/repository/UserRepository.java
@@ -0,0 +1,37 @@
+package com.ufmg.petclinic.repository;
+
+import com.ufmg.petclinic.model.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public class UserRepository {
+    private final List<User> users = new ArrayList<>();
+
+    public User create(User user) {
+        users.add(user);
+        return user;
+    }
+
+    public User delete(User user) {
+        users.remove(user);
+        return user;
+    }
+
+    public List<User> getAllUsers() {
+        return users;
+    }
+
+    public Optional<User> getUserById(UUID id) {
+        return users.stream().filter(user -> user.getId().equals(id)).findFirst();
+    }
+
+    public User update(User user, User newUser) {
+        users.set(users.indexOf(user), newUser);
+        return newUser;
+    }
+}
