897bde9b448c3c0241c665b70bf7a375cbfa7a33
Ana Sossai <analuizadsossai@hotmail.com>
2024-12-14 16:51:44 -0300

    feat: add constructor to class Clinic

--- src/main/java/com/ufmg/petclinic/model/Clinic.java
+++ src/main/java/com/ufmg/petclinic/model/Clinic.java
@@ -1,24 +1,32 @@
 package com.ufmg.petclinic.model;
 
+import java.util.UUID;
+
 public class Clinic {
  
-    private Long id;
+    private UUID id;
     private Long cnpj;
     private String name;
     private String address;
 
-    public Long getId() {
+    public Clinic(long cnpj, String name, String address) {
+        this.id = UUID.randomUUID();
+        this.cnpj = cnpj;
+        this.name = name;
+        this.address = address;
+    }
+
+    public UUID getId() {
         return id;
     }
     
-    public void setId(Long id) {
+    public void setId(UUID id) {
         this.id = id;
     }
 
     public Long getCnpj() {
         return cnpj;
     }
-
 
     public void setCnpj(Long cnpj) {
         this.cnpj = cnpj;
