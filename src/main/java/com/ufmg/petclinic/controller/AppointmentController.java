6354843572661d8afecfd7f8dbe3d3fa87f6e6f9
Ana Sossai <analuizadsossai@hotmail.com>
2024-12-15 10:29:10 -0300

    feat: add appointment controller

---
+++ src/main/java/com/ufmg/petclinic/controller/AppointmentController.java
@@ -0,0 +1,65 @@
+package com.ufmg.petclinic.controller;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.Optional;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ufmg.petclinic.model.Appointment;
+import com.ufmg.petclinic.service.AppointmentService;
+
+@RestController
+@RequestMapping("api/v1/appointment")
+public class AppointmentController {
+    private final AppointmentService service;
+
+    @Autowired
+    public AppointmentController(AppointmentService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<Appointment> createAppointment(@RequestBody Appointment appointment) {
+        Appointment createdAppointment = service.createAppointment(appointment);
+        return ResponseEntity.ok(createdAppointment);
+    }
+
+     @GetMapping("/{id}")
+    public ResponseEntity<Appointment> getAppointmentById(@PathVariable UUID id) {
+        Optional<Appointment> appointment = service.getAppointmentById(id);
+        return appointment.map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Appointment>> getAllAppointments() {
+        List<Appointment> appointments = service.getAllAppointments();
+        return ResponseEntity.ok(appointments);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAppointment(@PathVariable UUID id) {
+        service.deleteAppointment(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Appointment> updateAppointment(
+            @PathVariable UUID id,
+            @RequestBody Appointment updatedAppointment) {
+        Optional<Appointment> updated = service.updateAppointment(id, updatedAppointment);
+        return updated.map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+}
